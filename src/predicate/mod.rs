192adbd021eced8b76199232d32960407c1a5e3e
Stephen Lazaro <stephen@earnest.com>
2018-04-25 20:03:35 -0700

    Experiment with some number predicates

--- src/predicate/mod.rs
+++ src/predicate/mod.rs
@@ -1,2 +1,15 @@
+extern crate num;
+use self::num::Integer;
+
+pub fn is_even<A: Integer>(i: A) -> bool
+{
+    i.is_even()
+}
+
+pub fn is_odd<A: Integer>(i: A) -> bool
+{
+    !i.is_even()
+}
+
 pub mod constraint;
 pub mod generic;
